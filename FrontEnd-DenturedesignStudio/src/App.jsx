cb9d3c6a74146bfd215b9e7c28781ed3b84195a7
piyusam0133 <150885458+piyusam0133@users.noreply.github.com>
2024-06-17 07:06:20 +0000

    updating uploadPatientCase

--- FrontEnd-DenturedesignStudio/src/App.jsx
+++ FrontEnd-DenturedesignStudio/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './first-page/Homepage';
 import Loginpage from './logingpage/Logingpage';
 import Signup from './Signinpage/signup';
+import UploadPatientCases from './uploadPatientCases/UploadPatientCases';
 import './App.css';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route exact path='/' element={<Homepage />} />
         <Route exact path='/login' element={<Loginpage />} />
         <Route exact path='/signup' element={<Signup/>}/>
+        <Route path="/upload" element={<UploadPatientCases />} />
+
       </Routes>
     </Router>
   );
--- FrontEnd-DenturedesignStudio/src/uploadPatientCase/UploadPatientCase.jsx
+++ FrontEnd-DenturedesignStudio/src/uploadPatientCase/UploadPatientCase.jsx
@@ -11,15 +11,16 @@ function UploadPatientCases() {
 
   return (
     <>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" />
       <div className="UploadPatientCases">
         <div>
-          <h2 className='text1'>Create and Upload a Patient Case</h2>
-          <h1 className='text2'>step 1 : Select missing teeth
-            step 2 : Click Create & Upload!</h1>
+          <h2 className='CreateAPatientCase'>Create and Upload a Patient Case</h2>
+          <h1 className='steps'>
+            step 1 : Select missing teeth<br />
+            step 2 : Click Create & Upload!
+          </h1>
         </div>
         <div>
-          <button className='CreateAndUpload' onClick={()=>handleClick('/CreateAndUpload')}>Create & Upload</button>
+          <button className='button' onClick={() => handleClick('/CreateAndUpload')}>Create & Upload</button>
         </div>
       </div>
     </>
