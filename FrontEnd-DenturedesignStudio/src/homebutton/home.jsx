f3af166b21b79127bd73e86c11fa5dd7c2823b7f
piyusam0133 <e20346@eng.pdn.ac.lk>
2024-06-17 16:08:24 +0530

    home update

--- FrontEnd-DenturedesignStudio/src/homebutton/home.jsx
+++ FrontEnd-DenturedesignStudio/src/homebutton/home.jsx
@@ -1,14 +1,10 @@
-import home from "E:/e20-co225-Denture-Design-Studio/FrontEnd-DenturedesignStudio/src/homebutton/home.png";
-import "./home.css";
-
-function Home({ onclick }) {
-    return (
-        <div>
-            <button className="homebutton" onClick={onclick}>
-                <img src={home} alt="Home button" />
-            </button>
-        </div>
-    );
+import home from "E:/e20-co225-Denture-Design-Studio/FrontEnd-DenturedesignStudio/src/homebutton/home.png"
+import "./home.css"
+function Home({onClick}){
+    return(
+        <button className="homebutton" onClick={onClick}>
+            <img src={home} alt="Home" />
+        </button>
+    )
 }
-
-export default Home;
+export default Home
