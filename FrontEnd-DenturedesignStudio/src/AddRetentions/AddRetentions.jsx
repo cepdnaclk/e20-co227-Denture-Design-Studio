c41873903e51196d5dca9d682f95474f0d8f32ab
piyusam0133 <e20346@eng.pdn.ac.lk>
2024-06-20 00:10:25 +0530

    update

---
+++ FrontEnd-DenturedesignStudio/src/AddRetentions/AddRetentions.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './AddRetentions.css';
+import Home from '../homebutton/home';
+import BackComp from '../backComp/backComp';
+
+
+function AddRetensions() {
+  let navigate = useNavigate();
+
+  function handleClick(path) {
+    navigate(path);
+  }
+
+  return (
+    <>
+    <div className='designPage'>
+      <div className="AddRetensions">
+      <Home onClick={() => handleClick("/studenthome")}></Home>
+      <BackComp onClick={() => handleClick("/addRests")}></BackComp>
+        <div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" />
+        <div className="Questionbox">
+        </div>
+        <div className="teethBackground1">
+        <button className='addReciprocations' onClick={() => handleClick('/addReciprocations')}>
+          <div className="addRecipText">
+            <span className="addRecipText">Add Reciprocations</span>
+          </div>
+        </button>
+        
+        <ul className="retensions-list">
+            <li className="retensions" onClick={() => handleClick()}>Occlusally Approaching :</li>
+            <li className="retensions" onClick={() => handleClick()}>Gingivilly Approaching :</li>
+        </ul>
+  
+        </div>
+          <h2 className='AddSaddles'>Add Retentions</h2>
+          <h2 className='yourQuestion'>Your Question</h2>
+          <h1 className='yourCase'>Your Case :</h1>
+        </div>
+        
+      </div>
+      </div>    
+    </>
+  );
+}
+
+export default AddRetensions;
--- FrontEnd-DenturedesignStudio/src/homebutton/home.jsx
+++ FrontEnd-DenturedesignStudio/src/homebutton/home.jsx
@@ -1,10 +1,13 @@
-import home from "./home.png"
-import "./home.css"
-function Home({onClick}){
-    return(
+import React from 'react';
+import home from "./home.png";
+import "./home.css";
+
+function Home({ onClick }) {
+    return (
         <button className="homebutton" onClick={onClick}>
             <img src={home} alt="Home" />
         </button>
-    )
+    );
 }
-export default Home
+
+export default Home;
