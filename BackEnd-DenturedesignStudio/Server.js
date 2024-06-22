e9d13d2822025b692c3dae5049bea8bdae703f02
ravindu439 <e20439@eng.pdn.ac.lk>
2024-06-23 02:19:47 +0530

    back end 1st stage

---
+++ BackEnd-DenturedesignStudio/Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("mongodb connection success");  
+});
+
+app.listen(PORT, () => {
+    console.log(`server is up and running on port ${PORT}`);
+});
