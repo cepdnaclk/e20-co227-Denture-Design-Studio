e9dafc188c5bddad104b54662f6d8c8e3892c60e
ravindu439 <e20439@eng.pdn.ac.lk>
2024-07-26 10:43:38 +0530

    teeth comp update

--- FrontEnd-DenturedesignStudio/src/TeethComp/Teeth.jsx
+++ FrontEnd-DenturedesignStudio/src/TeethComp/Teeth.jsx
@@ -2,58 +2,84 @@ import React from "react";
 import "./Teeth.css";
 
 // Import all the images
-import t0 from './TeethImg/t0.gif';
-import t1 from './TeethImg/t1.gif';
-import t2 from './TeethImg/t2.gif';
-import t3 from './TeethImg/t3.gif';
-import t4 from './TeethImg/t4.gif';
-import t5 from './TeethImg/t5.gif';
-import t6 from './TeethImg/t6.gif';
-import t7 from './TeethImg/t7.gif';
-import t8 from './TeethImg/t8.gif';
-import t9 from './TeethImg/t9.gif';
-import t10 from './TeethImg/t10.gif';
-import t11 from './TeethImg/t11.gif';
-import t12 from './TeethImg/t12.gif';
-import t13 from './TeethImg/t13.gif';
-import t14 from './TeethImg/t14.gif';
-import t15 from './TeethImg/t15.gif';
-import t16 from './TeethImg/t16.gif';
-import t17 from './TeethImg/t17.gif';
-import t18 from './TeethImg/t18.gif';
-import t19 from './TeethImg/t19.gif';
-import t20 from './TeethImg/t20.gif';
-import t21 from './TeethImg/t21.gif';
-import t22 from './TeethImg/t22.gif';
-import t23 from './TeethImg/t23.gif';
-import t24 from './TeethImg/t24.gif';
-import t25 from './TeethImg/t25.gif';
-import t26 from './TeethImg/t26.gif';
-import t27 from './TeethImg/t27.gif';
-import t28 from './TeethImg/t28.gif';
-import t29 from './TeethImg/t29.gif';
-import t30 from './TeethImg/t30.gif';
-import t31 from './TeethImg/t31.gif';
+import t0 from "./TeethImg/t0.gif";
+import t1 from "./TeethImg/t1.gif";
+import t2 from "./TeethImg/t2.gif";
+import t3 from "./TeethImg/t3.gif";
+import t4 from "./TeethImg/t4.gif";
+import t5 from "./TeethImg/t5.gif";
+import t6 from "./TeethImg/t6.gif";
+import t7 from "./TeethImg/t7.gif";
+import t8 from "./TeethImg/t8.gif";
+import t9 from "./TeethImg/t9.gif";
+import t10 from "./TeethImg/t10.gif";
+import t11 from "./TeethImg/t11.gif";
+import t12 from "./TeethImg/t12.gif";
+import t13 from "./TeethImg/t13.gif";
+import t14 from "./TeethImg/t14.gif";
+import t15 from "./TeethImg/t15.gif";
+import t16 from "./TeethImg/t16.gif";
+import t17 from "./TeethImg/t17.gif";
+import t18 from "./TeethImg/t18.gif";
+import t19 from "./TeethImg/t19.gif";
+import t20 from "./TeethImg/t20.gif";
+import t21 from "./TeethImg/t21.gif";
+import t22 from "./TeethImg/t22.gif";
+import t23 from "./TeethImg/t23.gif";
+import t24 from "./TeethImg/t24.gif";
+import t25 from "./TeethImg/t25.gif";
+import t26 from "./TeethImg/t26.gif";
+import t27 from "./TeethImg/t27.gif";
+import t28 from "./TeethImg/t28.gif";
+import t29 from "./TeethImg/t29.gif";
+import t30 from "./TeethImg/t30.gif";
+import t31 from "./TeethImg/t31.gif";
+import image from "./image.png";
 
 const images = [
-  t0, t1, t2, t3, t4, t5, t6, t7, t8, t9,
-  t10, t11, t12, t13, t14, t15, t16, t17, t18, t19,
-  t20, t21, t22, t23, t24, t25, t26, t27, t28, t29,
-  t30, t31
+  t0,
+  t1,
+  t2,
+  t3,
+  t4,
+  t5,
+  t6,
+  t7,
+  t8,
+  t9,
+  t10,
+  t11,
+  t12,
+  t13,
+  t14,
+  t15,
+  t16,
+  t17,
+  t18,
+  t19,
+  t20,
+  t21,
+  t22,
+  t23,
+  t24,
+  t25,
+  t26,
+  t27,
+  t28,
+  t29,
+  t30,
+  t31,
 ];
 
 const Teeth = ({ click }) => {
   const handleClick = (index) => () => click(index);
 
   return (
-    <div className = "teethBackground1" >
+    <div className="teethBackground2">
       {Array.from({ length: 32 }, (_, index) => (
-        <button
-          key={index}
-          className="teeth-btn"
-          onClick={handleClick(index)}
-          style={{ backgroundImage: `url(${images[index]})` }}
-        />
+        <button key={index} className="teeth-btn" onClick={handleClick(index)}>
+          <img src={images[index]} alt={"teeth"} />
+        </button>
       ))}
     </div>
   );
