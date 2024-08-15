da25dc3ce003aeb1726393b4853b9b4dd4527eee
piyusam0133 <e20346@eng.pdn.ac.lk>
2024-08-15 15:02:56 +0530

    rest/clasp image folder and modified files

--- FrontEnd-DenturedesignStudio/src/AddRests/AddRests.jsx
+++ FrontEnd-DenturedesignStudio/src/AddRests/AddRests.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './AddRests.css';
 import Home from '../homebutton/home';
 import BackComp from '../backComp/backComp';
+import Teeth from "../TeethComp/Teeth";
 
 function AddRests() {
   let navigate = useNavigate();
@@ -21,24 +22,24 @@ function AddRests() {
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Salsa&display=swap" />
             <div className="Questionbox">
             </div>
-            <div className="teethBackground1">
+            <div>
               <button className='addRetentions' onClick={() => handleClick('/addRetentions')}>
                 <div className="addRetenText">
                   <span className="addRetenText">Add Retentions</span>
                 </div>
               </button>
-              
-              <ul className="rests-list">
-                <li id="occlusalRests" onClick={() => handleClick()}>Occlusal Rests :</li>
-                <li id="cingulumRests" onClick={() => handleClick()}>Cingulum Rests :</li>
-                <li id="incisalRests" onClick={() => handleClick()}>Incisal Rests :</li>
-              </ul>
-    
+
+            <div>
+              {/* Teeth component with interaction enabled */}
+              <Teeth click={(index) => console.log(`Clicked tooth ${index}`)} />
             </div>
+    
+          </div>
             <h2 className='AddRests'>Add Rests</h2>
             <h2 className='yourQuestion'>Your Question</h2>
             <h1 className='yourCase'>Your Case :</h1>
           </div>
+          
         </div>
       </div>    
     </>
