cb9e386173522125fda9883a7854c9d62563ab87
ravindu439 <e20439@eng.pdn.ac.lk>
2024-08-20 22:20:46 +0530

    imges file created

---
+++ FrontEnd-DenturedesignStudio/src/TeethComp/Claspsimages.jsx
@@ -0,0 +1,317 @@
+import c0ub from "./clasps/c0ub.gif";
+import c0ubL from "./clasps/c0ubL.gif";
+import c0ud from "./clasps/c0ud.gif";
+import c0udL from "./clasps/c0udL.gif";
+import c0um from "./clasps/c0um.gif";
+import c0umL from "./clasps/c0umL.gif";
+import c10ub from "./clasps/c10ub.gif";
+import c10ud from "./clasps/c10ud.gif";
+import c10um from "./clasps/c10um.gif";
+import c11ub from "./clasps/c11ub.gif";
+import c11ubL from "./clasps/c11ubL.gif";
+import c11ud from "./clasps/c11ud.gif";
+import c11udL from "./clasps/c11udL.gif";
+import c11um from "./clasps/c11um.gif";
+import c11umL from "./clasps/c11umL.gif";
+import c12ub from "./clasps/c12ub.gif";
+import c12ubL from "./clasps/c12ubL.gif";
+import c12ud from "./clasps/c12ud.gif";
+import c12udL from "./clasps/c12udL.gif";
+import c12um from "./clasps/c12um.gif";
+import c12umL from "./clasps/c12umL.gif";
+import c13ub from "./clasps/c13ub.gif";
+import c13ubL from "./clasps/c13ubL.gif";
+import c13ud from "./clasps/c13ud.gif";
+import c13udL from "./clasps/c13udL.gif";
+import c13um from "./clasps/c13um.gif";
+import c13umL from "./clasps/c13umL.gif";
+import c14ub from "./clasps/c14ub.gif";
+import c14ubL from "./clasps/c14ubL.gif";
+import c14ud from "./clasps/c14ud.gif";
+import c14udL from "./clasps/c14udL.gif";
+import c14um from "./clasps/c14um.gif";
+import c14umL from "./clasps/c14umL.gif";
+import c15ub from "./clasps/c15ub.gif";
+import c15ubL from "./clasps/c15ubL.gif";
+import c15ud from "./clasps/c15ud.gif";
+import c15udL from "./clasps/c15udL.gif";
+import c15um from "./clasps/c15um.gif";
+import c15umL from "./clasps/c15umL.gif";
+import c16ub from "./clasps/c16ub.gif";
+import c16ubL from "./clasps/c16ubL.gif";
+import c16ud from "./clasps/c16ud.gif";
+import c16udL from "./clasps/c16udL.gif";
+import c16um from "./clasps/c16um.gif";
+import c16umL from "./clasps/c16umL.gif";
+import c17ub from "./clasps/c17ub.gif";
+import c17ubL from "./clasps/c17ubL.gif";
+import c17ud from "./clasps/c17ud.gif";
+import c17udL from "./clasps/c17udL.gif";
+import c17um from "./clasps/c17um.gif";
+import c17umL from "./clasps/c17umL.gif";
+import c18ub from "./clasps/c18ub.gif";
+import c18ubL from "./clasps/c18ubL.gif";
+import c18ud from "./clasps/c18ud.gif";
+import c18udL from "./clasps/c18udL.gif";
+import c18um from "./clasps/c18um.gif";
+import c18umL from "./clasps/c18umL.gif";
+import c19ub from "./clasps/c19ub.gif";
+import c19ubL from "./clasps/c19ubL.gif";
+import c19ud from "./clasps/c19ud.gif";
+import c19udL from "./clasps/c19udL.gif";
+import c19um from "./clasps/c19um.gif";
+import c19umL from "./clasps/c19umL.gif";
+import c1ub from "./clasps/c1ub.gif";
+import c1ubL from "./clasps/c1ubL.gif";
+import c1ud from "./clasps/c1ud.gif";
+import c1udL from "./clasps/c1udL.gif";
+import c1um from "./clasps/c1um.gif";
+import c1umL from "./clasps/c1umL.gif";
+import c20ub from "./clasps/c20ub.gif";
+import c20ubL from "./clasps/c20ubL.gif";
+import c20ud from "./clasps/c20ud.gif";
+import c20udL from "./clasps/c20udL.gif";
+import c20um from "./clasps/c20um.gif";
+import c20umL from "./clasps/c20umL.gif";
+import c21ub from "./clasps/c21ub.gif";
+import c21ud from "./clasps/c21ud.gif";
+import c21um from "./clasps/c21um.gif";
+import c22ub from "./clasps/c22ub.gif";
+import c22ud from "./clasps/c22ud.gif";
+import c22um from "./clasps/c22um.gif";
+import c23ub from "./clasps/c23ub.gif";
+import c23ud from "./clasps/c23ud.gif";
+import c23um from "./clasps/c23um.gif";
+import c24ub from "./clasps/c24ub.gif";
+import c24ud from "./clasps/c24ud.gif";
+import c24um from "./clasps/c24um.gif";
+import c25ub from "./clasps/c25ub.gif";
+import c25ud from "./clasps/c25ud.gif";
+import c25um from "./clasps/c25um.gif";
+import c26ub from "./clasps/c26ub.gif";
+import c26ud from "./clasps/c26ud.gif";
+import c26um from "./clasps/c26um.gif";
+import c27ub from "./clasps/c27ub.gif";
+import c27ubL from "./clasps/c27ubL.gif";
+import c27ud from "./clasps/c27ud.gif";
+import c27udL from "./clasps/c27udL.gif";
+import c27um from "./clasps/c27um.gif";
+import c27umL from "./clasps/c27umL.gif";
+import c28ub from "./clasps/c28ub.gif";
+import c28ubL from "./clasps/c28ubL.gif";
+import c28ud from "./clasps/c28ud.gif";
+import c28udL from "./clasps/c28udL.gif";
+import c28um from "./clasps/c28um.gif";
+import c28umL from "./clasps/c28umL.gif";
+import c29ub from "./clasps/c29ub.gif";
+import c29ubL from "./clasps/c29ubL.gif";
+import c29ud from "./clasps/c29ud.gif";
+import c29udL from "./clasps/c29udL.gif";
+import c29um from "./clasps/c29um.gif";
+import c29umL from "./clasps/c29umL.gif";
+import c2ub from "./clasps/c2ub.gif";
+import c2ubL from "./clasps/c2ubL.gif";
+import c2ud from "./clasps/c2ud.gif";
+import c2udL from "./clasps/c2udL.gif";
+import c2um from "./clasps/c2um.gif";
+import c2umL from "./clasps/c2umL.gif";
+import c30ub from "./clasps/c30ub.gif";
+import c30ubL from "./clasps/c30ubL.gif";
+import c30ud from "./clasps/c30ud.gif";
+import c30udL from "./clasps/c30udL.gif";
+import c30um from "./clasps/c30um.gif";
+import c30umL from "./clasps/c30umL.gif";
+import c31ub from "./clasps/c31ub.gif";
+import c31ubL from "./clasps/c31ubL.gif";
+import c31ud from "./clasps/c31ud.gif";
+import c31udL from "./clasps/c31udL.gif";
+import c31um from "./clasps/c31um.gif";
+import c31umL from "./clasps/c31umL.gif";
+import c3ub from "./clasps/c3ub.gif";
+import c3ubL from "./clasps/c3ubL.gif";
+import c3ud from "./clasps/c3ud.gif";
+import c3udL from "./clasps/c3udL.gif";
+import c3um from "./clasps/c3um.gif";
+import c3umL from "./clasps/c3umL.gif";
+import c4ub from "./clasps/c4ub.gif";
+import c4ubL from "./clasps/c4ubL.gif";
+import c4ud from "./clasps/c4ud.gif";
+import c4udL from "./clasps/c4udL.gif";
+import c4um from "./clasps/c4um.gif";
+import c4umL from "./clasps/c4umL.gif";
+import c5ub from "./clasps/c5ub.gif";
+import c5ud from "./clasps/c5ud.gif";
+import c5um from "./clasps/c5um.gif";
+import c6ub from "./clasps/c6ub.gif";
+import c6ud from "./clasps/c6ud.gif";
+import c6um from "./clasps/c6um.gif";
+import c7ub from "./clasps/c7ub.gif";
+import c7ud from "./clasps/c7ud.gif";
+import c7um from "./clasps/c7um.gif";
+import c8ub from "./clasps/c8ub.gif";
+import c8ud from "./clasps/c8ud.gif";
+import c8um from "./clasps/c8um.gif";
+import c9ub from "./clasps/c9ub.gif";
+import c9ud from "./clasps/c9ud.gif";
+import c9um from "./clasps/c9um.gif";
+
+const ClaspImages = [
+  c0ub,
+  c0ubL,
+  c0ud,
+  c0udL,
+  c0um,
+  c0umL,
+  c10ub,
+  c10ud,
+  c10um,
+  c11ub,
+  c11ubL,
+  c11ud,
+  c11udL,
+  c11um,
+  c11umL,
+  c12ub,
+  c12ubL,
+  c12ud,
+  c12udL,
+  c12um,
+  c12umL,
+  c13ub,
+  c13ubL,
+  c13ud,
+  c13udL,
+  c13um,
+  c13umL,
+  c14ub,
+  c14ubL,
+  c14ud,
+  c14udL,
+  c14um,
+  c14umL,
+  c15ub,
+  c15ubL,
+  c15ud,
+  c15udL,
+  c15um,
+  c15umL,
+  c16ub,
+  c16ubL,
+  c16ud,
+  c16udL,
+  c16um,
+  c16umL,
+  c17ub,
+  c17ubL,
+  c17ud,
+  c17udL,
+  c17um,
+  c17umL,
+  c18ub,
+  c18ubL,
+  c18ud,
+  c18udL,
+  c18um,
+  c18umL,
+  c19ub,
+  c19ubL,
+  c19ud,
+  c19udL,
+  c19um,
+  c19umL,
+  c1ub,
+  c1ubL,
+  c1ud,
+  c1udL,
+  c1um,
+  c1umL,
+  c20ub,
+  c20ubL,
+  c20ud,
+  c20udL,
+  c20um,
+  c20umL,
+  c21ub,
+  c21ud,
+  c21um,
+  c22ub,
+  c22ud,
+  c22um,
+  c23ub,
+  c23ud,
+  c23um,
+  c24ub,
+  c24ud,
+  c24um,
+  c25ub,
+  c25ud,
+  c25um,
+  c26ub,
+  c26ud,
+  c26um,
+  c27ub,
+  c27ubL,
+  c27ud,
+  c27udL,
+  c27um,
+  c27umL,
+  c28ub,
+  c28ubL,
+  c28ud,
+  c28udL,
+  c28um,
+  c28umL,
+  c29ub,
+  c29ubL,
+  c29ud,
+  c29udL,
+  c29um,
+  c29umL,
+  c2ub,
+  c2ubL,
+  c2ud,
+  c2udL,
+  c2um,
+  c2umL,
+  c30ub,
+  c30ubL,
+  c30ud,
+  c30udL,
+  c30um,
+  c30umL,
+  c31ub,
+  c31ubL,
+  c31ud,
+  c31udL,
+  c31um,
+  c31umL,
+  c3ub,
+  c3ubL,
+  c3ud,
+  c3udL,
+  c3um,
+  c3umL,
+  c4ub,
+  c4ubL,
+  c4ud,
+  c4udL,
+  c4um,
+  c4umL,
+  c5ub,
+  c5ud,
+  c5um,
+  c6ub,
+  c6ud,
+  c6um,
+  c7ub,
+  c7ud,
+  c7um,
+  c8ub,
+  c8ud,
+  c8um,
+  c9ub,
+  c9ud,
+  c9um,
+];
+
+export default ClaspImages;
