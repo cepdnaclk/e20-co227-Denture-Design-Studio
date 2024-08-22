49311efa0e6bb2eace8479c14d2828274037b595
ravindu439 <e20439@eng.pdn.ac.lk>
2024-08-22 23:54:05 +0530

    another commit

--- FrontEnd-DenturedesignStudio/src/TeethComp/Teeth.jsx
+++ FrontEnd-DenturedesignStudio/src/TeethComp/Teeth.jsx
@@ -4,21 +4,12 @@ import "./Plate.css";
 import TeethImages from "./Teethimages";
 import RestImages from "./Restimages";
 import ClaspImages from "./Claspsimages";
-
-import UndercutsImages from "./Undercutimages";
-// Import all the images
-
 import PlateImages from "./PlatesImages";
-
 
 const Teeth = ({ disableSelection }) => {
   const [selectedTeeth, setSelectedTeeth] = useState(Array(32).fill(false));
   const [selectedRests, setSelectedRests] = useState(Array(56).fill(false));
   const [selectedPlate, setSelectedPlate] = useState(Array(40).fill(false));
-
-  const [selectedUnderCut, setSelectedUndercut] = useState(
-    Array(40).fill(false)
-  );
 
   const handleToothClick = (index) => {
     if (!disableSelection) {
@@ -39,16 +30,19 @@ const Teeth = ({ disableSelection }) => {
       });
     }
   };
-
-
   const handleUndercutClick = (index) => {
     if (!disableSelection) {
       setSelectedUndercut((prevState) => {
+        const newState = [...prevState];
+        newState[index] = !newState[index];
+        return newState;
+      });
+    }
+  };
 
   const handlePlateClick = (index) => {
     if (!disableSelection) {
       setSelectedPlate((prevState) => {
-
         const newState = [...prevState];
         newState[index] = !newState[index];
         return newState;
@@ -68,7 +62,7 @@ const Teeth = ({ disableSelection }) => {
         </button>
       ))}
 
-      {/*Array.from({ length: 56 }, (_, index) => (
+      {Array.from({ length: 56 }, (_, index) => (
         <button
           key={index}
           className={`rest-btn ${selectedRests[index] ? "selected" : ""}`}
@@ -77,20 +71,11 @@ const Teeth = ({ disableSelection }) => {
         >
           <img src={RestImages[index]} alt={`Rest ${index + 1}`} />
         </button>
-      ))*/}
+      ))}
 
       {Array.from({ length: 40 }, (_, index) => (
         <button
           key={index}
-
-          className={`undercut-btn ${
-            selectedUnderCut[index] ? "selected" : ""
-          }`}
-          id={`undercut-btn-${index + 1}`}
-          onClick={() => handleUndercutClick(index)}
-        >
-          <img src={UndercutsImages[index]} alt={`Undercut ${index + 1}`} />
-
           className={`plate-btn ${selectedPlate[index] ? "selected" : ""}`}
           id={`plate-btn-${index + 1}`}
           onClick={() => handlePlateClick(index)}
@@ -99,7 +84,18 @@ const Teeth = ({ disableSelection }) => {
           }}
         >
           <img src={PlateImages[index]} alt={`Plate ${index + 1}`} />
-
+        </button>
+      ))}
+      {Array.from({ length: 40 }, (_, index) => (
+        <button
+          key={index}
+          className={`undercut-btn ${
+            selectedUnderCut[index] ? "selected" : ""
+          }`}
+          id={`undercut-btn-${index + 1}`}
+          onClick={() => handleUndercutClick(index)}
+        >
+          <img src={UndercutsImages[index]} alt={`Undercut ${index + 1}`} />
         </button>
       ))}
     </div>
