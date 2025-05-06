0efa5d74197019f140aff23eab52c0eab4aacb63
ravindu439 <e20439@eng.pdn.ac.lk>
2025-05-06 23:59:04 +0530

    google v2

--- FrontEnd-DenturedesignStudio/src/main.jsx
+++ FrontEnd-DenturedesignStudio/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="74767315258-u4vkl3b06a8uka57o9295ct8irstkd6c.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="74767315258-p096i9js7a4f52n2mpcu8foo45nqem8g.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
   </React.StrictMode>
